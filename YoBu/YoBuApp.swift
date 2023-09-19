d4d453418319dd3cc7e430b18cd476e7b2a10e00
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-09-18 23:58:02 -0400

    chore: init YoBu 2.0

---
+++ YoBu/YoBuApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoBuApp.swift
+//  YoBu
+//
+//  Created by Singh, Harshdeep on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct YoBuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
