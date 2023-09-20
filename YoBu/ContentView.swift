7da0e80ecb18f8801fe13f54ff20c313f9f0081d
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2023-09-19 22:11:24 -0400

    chore: learning how to create a dynamic tab bar

--- YoBu/ContentView.swift
+++ YoBu/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedTabIndex = 0
+
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        TabView(selection: $selectedTabIndex) {
+            VStack {
+                Button(action: {
+                    print("Circular Button tapped")
+                    selectedTabIndex = Int.random(in: 0...2)
+                }) {
+                    Text("Call")
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.white)
+                        .background(Color.green)
+                        .clipShape(Circle())
+                }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            .tabItem {
+                Label("Home", systemImage: "house")
+            }
+            .tag(0)
+
+            Text("Search")
+                .tabItem {
+
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+                .tag(1)
+
+            Text("Notification")
+                .tabItem {
+                    Label("Notification", systemImage: "bell")
+                }
+                .tag(2)
+
+            Text("Settings")
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
+                .tag(3)
         }
-        .padding()
+
     }
 }
 
